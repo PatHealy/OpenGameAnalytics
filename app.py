fad9d6b90fcf62b349e138af3caddbb8a1ab79fa
Pat Healy <pwh5@pitt.edu>
2021-11-17 01:56:16 -0500

    Fully tested except action and experimentation

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # patwhealy.com
 # Last edited 11/1/21
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, Response, request
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -11,19 +11,25 @@ from http import HTTPStatus
 import random
 import string
 
-from models import User, User_Info, Play_Session, Play_Session_Start, Play_Session_Continue, Play_Session_End, Play_Action, Independent_Point, Dependent_Point
+from models import User, UserInfo, PlaySession, PlaySessionStart, PlaySessionContinue, PlaySessionEnd, PlayAction, IndependentPoint, DependentPoint
 
 #####################################################
 # Initialization
 #####################################################
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-	username="XXXXXXXXXXXXXXXX",
-	password="XXXXXXXXXXXXX",
-	hostname="XXXXXXXXXXXXXXX",
-	databasename="XXXXXXXXXXX",
-)
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+# 	username="XXXXXXXXXXXXXXXX",
+# 	password="XXXXXXXXXXXXX",
+# 	hostname="XXXXXXXXXXXXXXX",
+# 	databasename="XXXXXXXXXXX",
+# )
+# import os
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 api = Api(app)
 
@@ -37,23 +43,29 @@ def username_ganerator(size=30, chars=string.ascii_uppercase + string.digits):
 def auth(un, tk):
 	return check_password_hash(User.query.filter_by(username=un).first().token, tk)
 
+#recommend using a 6-digit number
+game_dictionary = {959742: "Example Game"}
+
 #####################################################
 # RESTful API
 #####################################################
 
 #/user
 class UserAPI(Resource):
-	def get(self):
-		username = username_ganerator()
-		token_plain = username_ganerator(size=50)
-		user = {'username': username, 'token': token_plain}
-		token = generate_password_hash(token_plain, method='pbkdf2:sha256', salt_length=8)
-		new_user = User(username=username, token=token)
-		db.session.add(new_user)
-		db.session.commit()
-		res = jsonify(user)
-		res.status = HTTPStatus.CREATED
-		return res
+	def get(self, game_id):
+		if game_id in game_dictionary.keys():
+			username = username_ganerator()
+			token_plain = username_ganerator(size=50)
+			user = {'username': username, 'token': token_plain}
+			token = generate_password_hash(token_plain, method='pbkdf2:sha256', salt_length=8)
+			new_user = User(username=username, token=token, game_name=game_dictionary[game_id])
+			db.session.add(new_user)
+			db.session.commit()
+			res = jsonify(user)
+			# res.status = HTTPStatus.CREATED
+			return res
+		else:
+			return Response(status=HTTPStatus.FORBIDDEN)
 
 	# def post(self):
 	# 	pass
@@ -66,41 +78,50 @@ class UserInfoAPI(Resource):
 		user_info = json_data['user_info']
 
 		if auth(user['username'], user['token']):
-			new_info = User_Info(attribute_name=user_info['attribute_name'], info=user_info['info'], fk_user_id=User.query.filter_by(username=un).first().pk_user_id)
+			new_info = UserInfo(attribute_name=user_info['attribute_name'], info=user_info['info'], fk_user_id=User.query.filter_by(username=un).first().pk_user_id)
 			db.session.add(new_info)
 			db.session.commit()
-			return make_response(status=HTTPStatus.CREATED)
+			return Response(status=HTTPStatus.CREATED)
 		else:
-			return make_response(status=HTTPStatus.FORBIDDEN)
+			return Response(status=HTTPStatus.FORBIDDEN)
 
 #/session
 class SessionAPI(Resource):
-	def get(self):
-		new_ses = Play_Session()
-		db.session.add(new_ses)
-		db.session.commit()
-		res = jsonify({'play_session_id': new_ses.pk_play_session_id})
-		res.status = HTTPStatus.CREATED
-		return res
+	def get(self, game_id):
+		if game_id in game_dictionary.keys():
+			new_ses = PlaySession()
+			db.session.add(new_ses)
+			db.session.commit()
+			res = jsonify({'play_session_id': new_ses.pk_play_session_id})
+			# res.status = HTTPStatus.CREATED
+			return res
+		else:
+			return Response(status=HTTPStatus.FORBIDDEN)
 
 #/session/user
 class SessionUserAPI(Resource):
 	def post(self):
-		json_data = request.get_json(force=True)
+		json_data = request.json
 		user = json_data['user']
 		play_session_id = json_data['play_session_id']
 
 		if auth(user['username'], user['token']):
-			play_ses = Play_Session.query.filter_by(pk_play_session_id=play_session_id).first()
-			play_ses.fk_user_id = User.query.filter_by(username=un).first().pk_user_id
+			play_ses = PlaySession.query.filter_by(pk_play_session_id=play_session_id).first()
+			play_ses.fk_user_id = User.query.filter_by(username=user['username']).first().pk_user_id
+			db.session.merge(play_ses)
+			#db.session.commit()
+			#db.session.commit()
+			db.session.commit()
 
-			new_ses_start = Play_Session_Start(fk_play_session_id=play_session_id)
+			
+			#db.session.flush()
 
+			new_ses_start = PlaySessionStart(fk_play_session_id=play_session_id)
 			db.session.add(new_ses_start)
 			db.session.commit()
-			return make_response(status=HTTPStatus.CREATED)
+			return Response(status=HTTPStatus.CREATED)
 		else:
-			return make_response(status=HTTPStatus.FORBIDDEN)
+			return Response(status=HTTPStatus.FORBIDDEN)
 
 #/session/continue
 class SessionContinueAPI(Resource):
@@ -110,12 +131,12 @@ class SessionContinueAPI(Resource):
 		play_session_id = json_data['play_session_id']
 
 		if auth(user['username'], user['token']):
-			new_ses_continue = Play_Session_Continue(fk_play_session_id=play_session_id)
+			new_ses_continue = PlaySessionContinue(fk_play_session_id=play_session_id)
 			db.session.add(new_ses_continue)
 			db.session.commit()
-			return make_response(status=HTTPStatus.CREATED)
+			return Response(status=HTTPStatus.CREATED)
 		else:
-			return make_response(status=HTTPStatus.FORBIDDEN)
+			return Response(status=HTTPStatus.FORBIDDEN)
 
 #/session/end
 class SessionEndAPI(Resource):
@@ -125,12 +146,12 @@ class SessionEndAPI(Resource):
 		play_session_id = json_data['play_session_id']
 
 		if auth(user['username'], user['token']):
-			new_ses_end = Play_Session_End(fk_play_session_id=play_session_id)
+			new_ses_end = PlaySessionEnd(fk_play_session_id=play_session_id)
 			db.session.add(new_ses_end)
 			db.session.commit()
-			return make_response(status=HTTPStatus.CREATED)
+			return Response(status=HTTPStatus.CREATED)
 		else:
-			return make_response(status=HTTPStatus.FORBIDDEN)
+			return Response(status=HTTPStatus.FORBIDDEN)
 
 #/session/action
 class ActionAPI(Resource):
@@ -141,12 +162,12 @@ class ActionAPI(Resource):
 		action = json_data['action']
 
 		if auth(user['username'], user['token']):
-			new_action = Play_Action(action_name=action['action_name'], info=action['info'], fk_user_id=User.query.filter_by(username=un).first().pk_user_id, fk_play_session_id=play_session_id)
+			new_action = PlayAction(action_name=action['action_name'], info=action['info'], fk_user_id=User.query.filter_by(username=un).first().pk_user_id, fk_play_session_id=play_session_id)
 			db.session.add(new_action)
 			db.session.commit()
-			return make_response(status=HTTPStatus.CREATED)
+			return Response(status=HTTPStatus.CREATED)
 		else:
-			return make_response(status=HTTPStatus.FORBIDDEN)
+			return Response(status=HTTPStatus.FORBIDDEN)
 
 #/experiment/independent
 class IndependentAPI(Resource):
@@ -156,12 +177,12 @@ class IndependentAPI(Resource):
 		point_data = json_data['independent_point']
 
 		if auth(user['username'], user['token']):
-			new_point = Independent_Point(attribute_name=point_data['attribute_name'], info=point_data['info'], fk_user_id=User.query.filter_by(username=un).first().pk_user_id)
+			new_point = IndependentPoint(attribute_name=point_data['attribute_name'], info=point_data['info'], fk_user_id=User.query.filter_by(username=un).first().pk_user_id)
 			db.session.add(new_point)
 			db.session.commit()
-			return make_response(status=HTTPStatus.CREATED)
+			return Response(status=HTTPStatus.CREATED)
 		else:
-			return make_response(status=HTTPStatus.FORBIDDEN)
+			return Response(status=HTTPStatus.FORBIDDEN)
 
 #/experiment/dependent
 class DependentAPI(Resource):
@@ -171,16 +192,16 @@ class DependentAPI(Resource):
 		point_data = json_data['dependent_point']
 
 		if auth(user['username'], user['token']):
-			new_point = Dependent_Point(attribute_name=point_data['attribute_name'], info=point_data['info'], fk_user_id=User.query.filter_by(username=un).first().pk_user_id)
+			new_point = DependentPoint(attribute_name=point_data['attribute_name'], info=point_data['info'], fk_user_id=User.query.filter_by(username=un).first().pk_user_id)
 			db.session.add(new_point)
 			db.session.commit()
-			return make_response(status=HTTPStatus.CREATED)
+			return Response(status=HTTPStatus.CREATED)
 		else:
-			return make_response(status=HTTPStatus.FORBIDDEN)
+			return Response(status=HTTPStatus.FORBIDDEN)
 
-api.add_resource(UserAPI, '/user')
+api.add_resource(UserAPI, '/user/<int:game_id>')
 api.add_resource(UserInfoAPI, '/user/info')
-api.add_resource(SessionAPI, '/session')
+api.add_resource(SessionAPI, '/session/<int:game_id>')
 api.add_resource(SessionUserAPI, '/session/user')
 api.add_resource(SessionContinueAPI, '/session/continue')
 api.add_resource(SessionEndAPI, '/session/end')
--- models.py
+++ models.py
@@ -2,46 +2,48 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-	pk_user_id = db.Column(db.Integer, primary_key=True)
+	pk_user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	username = db.Column(db.String)
 	token = db.Column(db.String)
+	game_name = db.Column(db.String)
 
-class User_Info(db.Model):
+class UserInfo(db.Model):
+	pk_user_info_id = db.Column(db.Integer, primary_key=True)
 	attribute_name = db.Column(db.String)
 	info = db.Column(db.Text)
 	fk_user_id = db.Column(db.Integer, db.ForeignKey('user.pk_user_id'))
 
-class Play_Session(db.Model):
+class PlaySession(db.Model):
 	pk_play_session_id = db.Column(db.Integer, primary_key=True)
 	fk_user_id = db.Column(db.Integer, db.ForeignKey('user.pk_user_id'))
 
-class Play_Session_Start(db.Model):
-	# pk_play_session_start_id = db.Column(db.Integer, primary_key=True)
+class PlaySessionStart(db.Model):
+	pk_play_session_start_id = db.Column(db.Integer, primary_key=True)
 	fk_play_session_id = db.Column(db.Integer, db.ForeignKey('play_session.pk_play_session_id'))
 
-class Play_Session_Continue(db.Model):
-	# pk_play_session_continue_id = db.Column(db.Integer, primary_key=True)
+class PlaySessionContinue(db.Model):
+	pk_play_session_continue_id = db.Column(db.Integer, primary_key=True)
 	fk_play_session_id = db.Column(db.Integer, db.ForeignKey('play_session.pk_play_session_id'))
 
-class Play_Session_End(db.Model):
-	# pk_play_session_end_id = db.Column(db.Integer, primary_key=True)
+class PlaySessionEnd(db.Model):
+	pk_play_session_end_id = db.Column(db.Integer, primary_key=True)
 	fk_play_session_id = db.Column(db.Integer, db.ForeignKey('play_session.pk_play_session_id'))
 
-class Play_Action(db.Model):
-	#pk_play_action_id = db.Column(db.Integer, primary_key=True)
+class PlayAction(db.Model):
+	pk_play_action_id = db.Column(db.Integer, primary_key=True)
 	action_name = db.Column(db.String)
 	info = db.Column(db.String)
 	fk_user_id = db.Column(db.Integer, db.ForeignKey('user.pk_user_id'))
 	fk_play_session_id = db.Column(db.Integer, db.ForeignKey('play_session.pk_play_session_id'))
 
-class Independent_Point(db.Model):
-	#pk_independent_point_id = db.Column(db.Integer, primary_key=True)
+class IndependentPoint(db.Model):
+	pk_independent_point_id = db.Column(db.Integer, primary_key=True)
 	attribute_name = db.Column(db.String)
 	info = db.Column(db.String)
 	fk_user_id = db.Column(db.Integer, db.ForeignKey('user.pk_user_id'))
 
-class Dependent_Point(db.Model):
-	#pk_dependent_point_id = db.Column(db.Integer, primary_key=True)
+class DependentPoint(db.Model):
+	pk_dependent_point_id = db.Column(db.Integer, primary_key=True)
 	attribute_name = db.Column(db.String)
 	info = db.Column(db.String)
 	fk_user_id = db.Column(db.Integer, db.ForeignKey('user.pk_user_id'))
