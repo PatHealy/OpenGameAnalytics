f9f00d0e61892505b9d9d937b50c8fadd70ac1f0
Pat Healy <pwh5@pitt.edu>
2022-11-15 01:17:49 -0500

    simplified query

--- queries.py
+++ queries.py
@@ -4,12 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def get_all_user_codes():
-	user_infos = pd.read_sql("select * from user_info where attribute_name='study_id' group by info, fk_user_id order by uploaded_at desc;", db.session.bind)
-	user_infos = user_infos[user_infos["attribute_name"] == "study_id"]
-
-	user_infos = user_infos[["info", "created_at", "uploaded_at", "fk_user_id"]]
-	user_infos = user_infos.rename({"info":"study_id", "fk_user_id":"user_id"})
-
+	user_infos = pd.read_sql("select info as study_id, created_at, uploaded_at, fk_user_id as user_id from user_info where attribute_name='study_id' group by info, fk_user_id order by uploaded_at desc;", db.session.bind)
 	table_label = "Users by study_id (only includes users with study_id assigned)"
 	table_header = ["study_id", "created_at", "uploaded_at", "user_id"]
 	table_data = [[row["study_id"], row["created_at"], row["uploaded_at"], row["user_id"]] for index, row in user_infos.iterrows()]
