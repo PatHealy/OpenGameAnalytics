c5e089a5b448d78dcbd08eba379ea91c2050c2e0
Pat Healy <pwh5@pitt.edu>
2022-11-15 02:04:24 -0500

    updated obvious db_session issue

--- queries.py
+++ queries.py
@@ -25,7 +25,8 @@ def get_user_sessions(user_id):
 			where fk_user_id = %(user_id)s group by pk_play_session_id
 			order by pk_play_session_id desc;
 		""",
-	    params={"user_id":user_id}
+	    params={"user_id":user_id},
+	    db.session.bind
 	)
 
 	table_label = "Sessions of user #" + str(user_id)
